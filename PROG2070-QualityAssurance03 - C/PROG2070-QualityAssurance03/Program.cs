d81c14433fcfffe238df018bcdf5d44d7c147efd
Derek Fusari <derek.fusari@gmail.com>
2017-03-23 22:27:28 -0400

    Add files via upload

---
+++ PROG2070-QualityAssurance03 - C/PROG2070-QualityAssurance03/Program.cs
@@ -0,0 +1,137 @@
+﻿using PROG2070_QualityAssurance03_C;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Programmer: Derek Fusari
+/// Software Quality Assurance Assignment 3
+/// Date: Thursday March 23rd/2017
+/// </summary>
+
+namespace PROG2070_QualityAssurance03_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMenu();
+        }
+
+        /// <summary>
+        /// Displays the menu and handles all 
+        /// interactions between the user and the menu
+        /// </summary>
+        public static void DisplayMenu()
+        {
+            int menuOption = 0;
+            double value = 0;
+            string convertFrom = "";
+            string convertTo = "";
+            double answer = 0;
+
+            Console.WriteLine("******MAIN MENU******");
+            Console.WriteLine("1. Convert a Time");
+            Console.WriteLine("2. Help Menu");
+            Console.WriteLine("3. Exit Program");
+
+            try
+            {
+                menuOption = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+            }
+
+            if (menuOption == 1)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Enter a time "
+                    + "value you wish to convert.");
+                Console.WriteLine("");
+
+                try
+                {
+                    value = double.Parse(Console.ReadLine());
+                    if (value <= 0)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Please enter " 
+                            + "a time greater than zero.");
+                        Console.WriteLine("");
+                        DisplayMenu();
+                    }
+
+                    Console.WriteLine("");
+                    Console.WriteLine("What is the unit if time you entered?");
+                    Console.WriteLine("");
+
+                    convertFrom = Console.ReadLine();
+
+                    Console.WriteLine("");
+                    Console.WriteLine("What is the unit " 
+                        + "if time you wish to convert to?");
+                    Console.WriteLine("");
+
+                    convertTo = Console.ReadLine();
+
+                    //Calls the Convert method to determine the 
+                    //converted value the user wants
+                    answer = TimeConversion.Convert(
+                        value, convertFrom, convertTo);
+
+                    if (answer != -1)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine(value + " " + convertFrom + 
+                            " is equal to " + answer + " " + convertTo);
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Incorect Time Unit");
+                        Console.WriteLine("");
+                    }
+                    DisplayMenu();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Please enter a proper time");
+                    Console.WriteLine("");
+                    DisplayMenu();
+                }
+            }
+            else if (menuOption == 2)
+            {
+                //A help menu that tells the user all the valid input
+                //for units of time
+                Console.WriteLine("");
+                Console.WriteLine("When entering a unit of time, " 
+                    + "choose any of these options: ");
+                Console.WriteLine("Seconds, seconds, S or s");
+                Console.WriteLine("Minutes, minutes, M or m");
+                Console.WriteLine("Hours, hours, H or h");
+                Console.WriteLine("Days, days, D or d");
+                Console.WriteLine("");
+
+                DisplayMenu();
+            }
+            else if (menuOption == 3)
+            {
+                System.Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("You did not " 
+                    + "enter a viable menu option.");
+                Console.WriteLine("");
+                DisplayMenu();
+            }
+        }
+    }
+}
---
+++ PROG2070-QualityAssurance03 - C/PROG2070-QualityAssurance03/TimeConversion.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070_QualityAssurance03_C
+{
+    class TimeConversion
+    {
+        /// <summary>
+        /// Converts the time the user enters to the unit specified
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="convertFrom">Conver time From</param>
+        /// <param name="convertTo">Convert time To</param>
+        /// <returns></returns>
+        public static double Convert(
+            double value, string convertFrom, string convertTo)
+        {
+            double answer = 0;
+            double multiplier = 0;
+
+
+            convertFrom = ModifyInput(convertFrom);
+            convertTo = ModifyInput(convertTo);
+
+            //If the user enters an incorrect time unit, the answer
+            // is ser to -1 so It is easy to determine there was an error
+            //in the program method
+            if (convertFrom == "Incorrect Time Unit")
+            {
+                answer = -1;
+            }
+            else if (convertTo == "Incorrect Time Unit")
+            {
+                answer = -1;
+            }
+            else
+            {
+                //Grabs the correct multiplier for calculating conversion
+                multiplier = GetMultiplier(convertFrom, convertTo);
+                answer = value * multiplier;
+            }
+            
+            return answer;
+        }
+        /// <summary>
+        /// Modify Input takes the users entry and changes it to 
+        /// a basic word to shorten code elsewhere and give leeway
+        /// to the users input
+        /// </summary>
+        /// <param name="input">User input of time unit</param>
+        /// <returns></returns>
+        private static string ModifyInput(string input)
+        {
+            if (input == "s" || input == "S" || 
+                input == "Seconds" || input == "seconds")
+            {
+                input = "seconds";
+            }
+            else if (input == "m" || input == "M" || 
+                input == "Minutes" || input == "minutes")
+            {
+                input = "minutes";
+            }
+            else if (input == "h" || input == "H" || 
+                input == "Hours" || input == "hours")
+            {
+                input = "hours";
+            }
+            else if (input == "d" || input == "D" ||
+                input == "Days" || input == "days")
+            {
+                input = "days";
+            }
+            else
+            {
+                input = "Incorrect Time Unit";
+            }
+            return input;
+        }
+        /// <summary>
+        /// Checks the convertFrom unit and the
+        /// convertTo unit and sets the multiplier
+        /// to calculate the proper conversion
+        /// </summary>
+        /// <param name="convertFrom">Convert time from</param>
+        /// <param name="convertTo">Conver time to</param>
+        /// <returns></returns>
+        private static double GetMultiplier(
+            string convertFrom, string convertTo)
+        {
+            double multiplier = 0;
+            
+            if (convertFrom == "seconds"
+                && convertTo == "minutes")
+            {
+                multiplier = 0.01666666666666666666;
+            }
+            else if (convertFrom == "seconds"
+                && convertTo == "hours")
+            {
+                multiplier = 0.000277778;
+            }
+            else if (convertFrom == "seconds"
+                && convertTo == "days")
+            {
+                multiplier = 1.1574083333e-5;
+            }
+            else if (convertFrom == "seconds"
+                && convertTo == "seconds")
+            {
+                multiplier = 1;
+            }
+            else if (convertFrom == "minutes"
+                && convertTo == "seconds")
+            {
+                multiplier = 60;
+            }
+            else if (convertFrom == "minutes"
+                && convertTo == "hours")
+            {
+                multiplier = 0.016666666666666666;
+            }
+            else if (convertFrom == "minutes"
+                && convertTo == "days")
+            {
+                multiplier = 0.00069444444444444444;
+            }
+            else if (convertFrom == "minutes" 
+                && convertTo == "minutes")
+            {
+                multiplier = 1;
+            }
+            else if (convertFrom == "hours"
+                && convertTo == "seconds")
+            {
+                multiplier = 3600;
+            }
+            else if (convertFrom == "hours" 
+                && convertTo == "minutes")
+            {
+                multiplier = 60;
+            }
+            else if (convertFrom == "hours"
+                && convertTo == "days")
+            {
+                multiplier = 0.041666666666666666;
+            }
+            else if (convertFrom == "hours"
+                && convertTo == "hours")
+            {
+                multiplier = 1;
+            }
+            else if (convertFrom == "days" 
+                && convertTo == "seconds")
+            {
+                multiplier = 86400;
+            }
+            else if (convertFrom == "days"
+                && convertTo == "minutes")
+            {
+                multiplier = 1440;
+            }
+            else if (convertFrom == "days"
+                && convertTo == "hours")
+            {
+                multiplier = 24;
+            }
+            else if (convertFrom == "days"
+                && convertTo == "days")
+            {
+                multiplier = 1;
+            }
+            return multiplier;
+        }
+    }
+}
